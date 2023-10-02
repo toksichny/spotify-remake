e4681b23705cdd4c6e9344e0274301ff482384ec
toksichny <netcool322@yandex.ru>
2023-10-02 21:12:18 +0300

    final

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "cyjvkmdyiuhdwyjabmya.supabase.co"
+        ]
+    },
+    experimental: {
+        appDir: true,
+    },
+}
 
 module.exports = nextConfig
